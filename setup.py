b22007da532d587947f18a4c5001d458318ab450
Artem Razumov <airplaneless@yandex.ru>
2019-08-20 19:21:34 +0500

    setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
+
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
+
 
 setup(name='hallgerd',
       version='0.1.0',
+      description='Deep learning framework for OpenCL',
+      author='Artem Razumov',
+      author_email='airplaneless@yandex.ru',
+      url='https://github.com/Airplaneless/Hallgerd',
+      platforms=['any'],
+      install_requires=['pyopencl', 'numpy>=1.13.0', 'tqdm', 'scikit-learn>=0.21.3', 'pybind11', 'mako'],
       packages=['hallgerd', 'gunnar', 'gunnar.kernels'],
       package_data={'gunnar.kernels': ['*.c']},
-      data_files=[('hallgerd', ['README.md']), ('gunnar', ['README.md'])],
+      data_files=[('gunnar', ['README.md']), ('', ['LICENSE', 'README.md'])],
+      license=license
       )
